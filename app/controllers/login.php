07d693e253af12f7eab2c9975837acbaf2860bcd
LKartavya <147118099+LKartavya@users.noreply.github.com>
2025-06-19 03:53:21 +0000

    update login file

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -3,15 +3,16 @@
 class Login extends Controller {
 
 		public function index() {		
-			$this->view('login/index');
+			$fail = isset($_GET['fail']);
+			$locked = isset($_GET['locked']);
+			$this->view('login/index', ['fail' => $fail, 'locked' => $locked]);
 		}
 
 		public function verify(){
 			$username = $_REQUEST['username'];
 			$password = $_REQUEST['password'];
-
 			$user = $this->model('User');
-			$user->authenticate($username, $password); 
+			$user->authenticate($username, $password);
 		}
 
 }
