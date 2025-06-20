26346dce5278ba1f9545e3f81205c58648a5b4b9
LKartavya <147118099+LKartavya@users.noreply.github.com>
2025-06-20 02:38:31 +0000

    changes IN HOME FILE

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,10 +4,10 @@ class Home extends Controller {
 
     public function index() {
         require_once 'app/models/User.php';
-        $user = new User();
-        $user->test();
-    //  $user = $this->model('User');
-    //  $data = $user->test();
+       // $user = new User();
+     //   $user->test();
+      $user = $this->model('User');
+      $data = $user->test();
 
         $this->view('home/index');
         die;
